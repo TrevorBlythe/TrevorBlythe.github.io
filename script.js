f6ea16bd660edf6c908ac0562a3d080876017bb0
TrevorBlythe <77030926+TrevorBlythe@users.noreply.github.com>
2022-11-16 18:34:17 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//play the video of me spinning at fast speed
+// if (document.getElementById("myVideo")) document.getElementById("myVideo").playbackRate = 3.0;
+
+//do the animation with my stats
+setTimeout(() => {
+	document.getElementById("statOne").style.animation = "extend 1s forwards 1";
+}, 1000);
+
+setTimeout(() => {
+	document.getElementById("statTwo").style.animation = "extend 1s forwards 1";
+}, 1200);
+
+setTimeout(() => {
+	document.getElementById("statThree").style.animation = "extend 1s forwards 1";
+}, 1400);
